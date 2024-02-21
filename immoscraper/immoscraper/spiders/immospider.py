572db4d4f614432e57702fe9c5da9568798b73ba
Viktor Cosaert <viktor.cosaert@gmail.com>
2024-02-21 11:51:56 +0100

    updated spiders

--- immoscraper/immoscraper/spiders/immospider.py
+++ immoscraper/immoscraper/spiders/immospider.py
@@ -1,77 +1,288 @@
 
-import scrapy
+import scrapy  # pip install scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from multiprocessing import Process, freeze_support  # used to crawl multiple spiders + handle freeze error
+from bs4 import BeautifulSoup  # pip install bs4
+import time
+import pandas as pd  # pip install scrapy
+import json
+
+# from ./path/to/spidermodule import SpiderClass, SpiderClass  # -> Import the spider classes
+
 
 # activate venv
-class ImmowebSpider(scrapy.Spider):
+
+
+
+
+class HouseSpider(scrapy.Spider):
     name = 'Immoweb_houses'
     start_urls = ['https://www.immoweb.be/en/search/house/for-sale?countries=BE&page=1']
     times = 0
+    looptime = 10
+    
     def parse(self, response):
-            for houses in response.css('div.card--result__body'):
-                page_link = houses.css('a.card__title-link').attrib['href']
-                if 'real-estate-project' not in page_link:
-                    yield {
-                        'link': page_link,
-                    }
-            self.times += 1
-            next_page = f'https://www.immoweb.be/en/search/house/for-sale?countries=BE&page={self.times}'
-            if self.times < 2:
-                yield response.follow(next_page, callback=self.parse)
-            else:
-                self.logger.info('\n*\n   ~~~Last page finished.~~~\n*\n')
+        for houses in response.css('div.card--result__body'):
+            page_link = houses.css('a.card__title-link::attr(href)').get()
+            if 'real-estate-project' not in page_link:
+                yield response.follow(page_link, callback=self.parse_link)
+        self.times += 1
+        next_page = f'https://www.immoweb.be/en/search/house/for-sale?countries=BE&page={self.times}'
+        if self.times < self.looptime:  # Adjust this as needed for how many pages you want to scrape
+            yield response.follow(next_page, callback=self.parse)
+        else:
+            self.logger.info('\n*\n   ~~~Last page finished.~~~\n*\n')
 
-class ImmowebSpider(scrapy.Spider):
+    def parse_link(self, response):
+        # Inserting Bear's code:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        script_tag = soup.find("script", string=lambda text: text and "window.classified =" in text)  # Find the script tag containing "window.classified = "
+        script_content = script_tag.string  # Get the content of the script tag
+        start_index = script_content.find("window.classified = ") + len("window.classified = ")  # Find the start index of the JSON data
+        str_data = script_content[start_index:].strip().rstrip(";")  # Extract the JSON data as a string
+        dict_data = json.loads(str_data)  # Parse the JSON data into a dictionary
+        
+        filtered_dict_data = self.json_extraction(dict_data, response.url)  # Little adjustment to fit in my code
+
+        yield filtered_dict_data #returns filtered dict data to the scrapy pipeline
+
+    def json_extraction(self, dict_data, url):
+        selected_values = [  # Define a list of tuples specifying the desired attributes and their corresponding JSON paths
+           ("ID", "id"),
+           ("Street", "property.location.street"),
+           ("HouseNumber", "property.location.number"),
+           ("Box", "property.location.box"),
+           ("Floor", "property.location.floor"),
+           ("City", "property.location.locality"),
+           ("PostalCode", "property.location.postalCode"),
+           ("Region", "property.location.regionCode"),
+           ("District", "property.location.district"),
+           ("Province", "property.location.province"),
+           ("PropertyType", "property.type"),
+           ("PropertySubType", "property.subtype"),
+           ("Price", "price.mainValue"),
+           ("SaleType", "price.type"),
+           ("ConstructionYear", "property.building.constructionYear"),
+           ("BedroomCount", "property.bedroomCount"),
+           ("LivingArea", "property.netHabitableSurface"),
+           ("KitchenType", "property.kitchen.type"),
+           ("Furnished", "transaction.sale.isFurnished"),
+           ("Fireplace", "property.fireplaceExists"),
+           ("Terrace", "property.hasTerrace"),
+           ("TerraceArea", "property.terraceSurface"),
+           ("Garden", "property.hasGarden"),
+           ("GardenArea", "property.land.surface"),
+           ("Facades", "property.building.facadeCount"),
+           ("SwimmingPool", "property.hasSwimmingPool"),
+           ("Condition", "property.building.condition"),
+           ("EPCScore", "transaction.certificates.epcScore"),
+           ("Latitude", "property.location.latitude"),
+           ("Longitude", "property.location.longitude"),
+           ("PropertyUrl", "url")
+        ]
+        filtered_dict_data = {"PropertyUrl": url}
+        for new_key, old_key in selected_values:
+            nested_keys = old_key.split(".")
+            value = dict_data
+            for nested_key in nested_keys:
+                if isinstance(value, dict) and nested_key in value:
+                    value = value[nested_key]
+                else:
+                    value = None
+                    break
+            filtered_dict_data[new_key] = value
+        return filtered_dict_data
+
+class ApartmentSpider(scrapy.Spider):
     name = 'Immoweb_apartments'
     start_urls = ['https://www.immoweb.be/en/search/apartment/for-sale?countries=BE&page=1']
     times = 0
-    def parse(self, response):
-            for houses in response.css('div.card--result__body'):
-                page_link = houses.css('a.card__title-link').attrib['href']
-                if 'real-estate-project' not in page_link:
-                    yield {
-                        'link': page_link,
-                    }
-            self.times += 1
-            next_page = f'https://www.immoweb.be/en/search/apartment/for-sale?countries=BE&page={self.times}'
-            if self.times < 2:
-                yield response.follow(next_page, callback=self.parse)
-            else:
-                self.logger.info('\n*\n   ~~~Last page finished.~~~\n*\n')
-
-# When inside crawler folder $ scrapy crawl [name] -O Immo[name].json -> or .csv => creates an output file
-                
-
-from bs4 import BeautifulSoup
-import scrapy
-
-class ImmowebSpider(scrapy.Spider):
-    name = 'Immoweb_apartments_upgraded'
-    start_urls = ['https://www.immoweb.be/en/search/apartment/for-sale?countries=BE&page=1']
-    times = 0
+    looptime = 10
 
     def parse(self, response):
         for houses in response.css('div.card--result__body'):
             page_link = houses.css('a.card__title-link::attr(href)').get()
             if 'real-estate-project' not in page_link:
-                # Instead of yielding the link, follow it to scrape more details using another callback
-                yield response.follow(page_link, callback=self.parse_apartment)
+                yield response.follow(page_link, callback=self.parse_link)
         self.times += 1
         next_page = f'https://www.immoweb.be/en/search/apartment/for-sale?countries=BE&page={self.times}'
-        if self.times < 2:  # Adjust this as needed for how many pages you want to scrape
+        if self.times < self.looptime:  # Adjust this as needed for how many pages you want to scrape
             yield response.follow(next_page, callback=self.parse)
         else:
             self.logger.info('\n*\n   ~~~Last page finished.~~~\n*\n')
 
-    def parse_apartment(self, response):
-        # Use BeautifulSoup to parse the apartment details
+    def parse_link(self, response):
+        # Inserting Bear's code:
         soup = BeautifulSoup(response.text, 'html.parser')
-        # Example of extracting the title with BeautifulSoup
-        id = soup.find('id').get_text(strip=True) if soup.find('id') else 'No id found'
-        price = ('[insert way to do that]')
-        # Extract other details as needed
-        # ...
-        yield {
-            'id': id,
-            'price': price,
-            # Include other extracted details here
-        }
+        script_tag = soup.find("script", string=lambda text: text and "window.classified =" in text)  # Find the script tag containing "window.classified = "
+        script_content = script_tag.string  # Get the content of the script tag
+        start_index = script_content.find("window.classified = ") + len("window.classified = ")  # Find the start index of the JSON data
+        str_data = script_content[start_index:].strip().rstrip(";")  # Extract the JSON data as a string
+        dict_data = json.loads(str_data)  # Parse the JSON data into a dictionary
+        
+        filtered_dict_data = self.json_extraction(dict_data, response.url)  # Little adjustment to fit in my code
+
+        yield filtered_dict_data #returns filtered dict data to the scrapy pipeline
+
+    def json_extraction(self, dict_data, url):
+        selected_values = [  # Define a list of tuples specifying the desired attributes and their corresponding JSON paths
+           ("ID", "id"),
+           ("Street", "property.location.street"),
+           ("HouseNumber", "property.location.number"),
+           ("Box", "property.location.box"),
+           ("Floor", "property.location.floor"),
+           ("City", "property.location.locality"),
+           ("PostalCode", "property.location.postalCode"),
+           ("Region", "property.location.regionCode"),
+           ("District", "property.location.district"),
+           ("Province", "property.location.province"),
+           ("PropertyType", "property.type"),
+           ("PropertySubType", "property.subtype"),
+           ("Price", "price.mainValue"),
+           ("SaleType", "price.type"),
+           ("ConstructionYear", "property.building.constructionYear"),
+           ("BedroomCount", "property.bedroomCount"),
+           ("LivingArea", "property.netHabitableSurface"),
+           ("KitchenType", "property.kitchen.type"),
+           ("Furnished", "transaction.sale.isFurnished"),
+           ("Fireplace", "property.fireplaceExists"),
+           ("Terrace", "property.hasTerrace"),
+           ("TerraceArea", "property.terraceSurface"),
+           ("Garden", "property.hasGarden"),
+           ("GardenArea", "property.land.surface"),
+           ("Facades", "property.building.facadeCount"),
+           ("SwimmingPool", "property.hasSwimmingPool"),
+           ("Condition", "property.building.condition"),
+           ("EPCScore", "transaction.certificates.epcScore"),
+           ("Latitude", "property.location.latitude"),
+           ("Longitude", "property.location.longitude"),
+           ("PropertyUrl", "url")
+        ]
+        filtered_dict_data = {"PropertyUrl": url}
+        for new_key, old_key in selected_values:
+            nested_keys = old_key.split(".")
+            value = dict_data
+            for nested_key in nested_keys:
+                if isinstance(value, dict) and nested_key in value:
+                    value = value[nested_key]
+                else:
+                    value = None
+                    break
+            filtered_dict_data[new_key] = value
+        return filtered_dict_data
+    
+
+class HouseApartmentSpider(scrapy.Spider):
+    name = 'Immoweb_houses'
+    start_urls = ['https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&page=1']
+    times = 0
+    looptime = 10
+    
+    def parse(self, response):
+        for houses in response.css('div.card--result__body'):
+            page_link = houses.css('a.card__title-link::attr(href)').get()
+            if 'real-estate-project' not in page_link:
+                yield response.follow(page_link, callback=self.parse_link)
+        self.times += 1
+        next_page = f'https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&page={self.times}'
+        if self.times < self.looptime:  # Adjust this as needed for how many pages you want to scrape
+            yield response.follow(next_page, callback=self.parse)
+        else:
+            self.logger.info('\n*\n   ~~~Last page finished.~~~\n*\n')
+
+    def parse_link(self, response):
+        # Inserting Bear's code:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        script_tag = soup.find("script", string=lambda text: text and "window.classified =" in text)  # Find the script tag containing "window.classified = "
+        script_content = script_tag.string  # Get the content of the script tag
+        start_index = script_content.find("window.classified = ") + len("window.classified = ")  # Find the start index of the JSON data
+        str_data = script_content[start_index:].strip().rstrip(";")  # Extract the JSON data as a string
+        dict_data = json.loads(str_data)  # Parse the JSON data into a dictionary
+        
+        filtered_dict_data = self.json_extraction(dict_data, url=response.url)  # Little adjustment to fit in my code
+
+        yield filtered_dict_data #returns filtered dict data to the scrapy pipeline
+
+    def json_extraction(self, dict_data, url):
+        selected_values = [  # Define a list of tuples specifying the desired attributes and their corresponding JSON paths
+           ("ID", "id"),
+           ("Street", "property.location.street"),
+           ("HouseNumber", "property.location.number"),
+           ("Box", "property.location.box"),
+           ("Floor", "property.location.floor"),
+           ("City", "property.location.locality"),
+           ("PostalCode", "property.location.postalCode"),
+           ("Region", "property.location.regionCode"),
+           ("District", "property.location.district"),
+           ("Province", "property.location.province"),
+           ("PropertyType", "property.type"),
+           ("PropertySubType", "property.subtype"),
+           ("Price", "price.mainValue"),
+           ("SaleType", "price.type"),
+           ("ConstructionYear", "property.building.constructionYear"),
+           ("BedroomCount", "property.bedroomCount"),
+           ("LivingArea", "property.netHabitableSurface"),
+           ("KitchenType", "property.kitchen.type"),
+           ("Furnished", "transaction.sale.isFurnished"),
+           ("Fireplace", "property.fireplaceExists"),
+           ("Terrace", "property.hasTerrace"),
+           ("TerraceArea", "property.terraceSurface"),
+           ("Garden", "property.hasGarden"),
+           ("GardenArea", "property.land.surface"),
+           ("Facades", "property.building.facadeCount"),
+           ("SwimmingPool", "property.hasSwimmingPool"),
+           ("Condition", "property.building.condition"),
+           ("EPCScore", "transaction.certificates.epcScore"),
+           ("Latitude", "property.location.latitude"),
+           ("Longitude", "property.location.longitude"),
+           ("PropertyUrl", "url")
+        ]
+        filtered_dict_data = {"PropertyUrl": url}
+        for new_key, old_key in selected_values:
+            nested_keys = old_key.split(".")
+            value = dict_data
+            for nested_key in nested_keys:
+                if isinstance(value, dict) and nested_key in value:
+                    value = value[nested_key]
+                else:
+                    value = None
+                    break
+            filtered_dict_data[new_key] = value
+        return filtered_dict_data
+
+
+def process_setup(spider_class, output_file):
+    # Sets up out a spider's settings & the intended output
+        settings = get_project_settings()  #-> get the configurations like it would run from command line
+        settings.set('FEEDS', {
+            output_file: {
+                'format': 'csv',
+                'overwrite': True,
+            },
+        })
+        process = CrawlerProcess(settings=settings)
+        process.crawl(spider_class)
+        process.start()
+
+def run_spider(spider_class, output_file):
+    p = Process(target=process_setup, args=(spider_class, output_file))
+    p.start()
+    p.join()
+
+def merge_csv_files(output='merged_data.csv'):
+    apartments = pd.read_csv('apartments.csv')
+    houses = pd.read_csv('houses.csv')
+    merged = pd.concat([apartments, houses])
+    merged.to_csv(output, index=True)
+
+
+if __name__ == '__main__':
+    freeze_support()
+    start_time = time.time()
+    # run_spider(ApartmentSpider, 'apartments.csv')
+    # run_spider(HouseSpider, 'houses.csv')
+    run_spider(HouseApartmentSpider, 'all.csv')
+    # merge_csv_files()
+    end_time = time.time()
+    print(f"Spiders finished in {end_time - start_time} seconds.")
